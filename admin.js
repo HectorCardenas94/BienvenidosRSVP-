c680f99f52e6d0b77ec336b78660fc7817a1751d
HectorCardenas94 <cardenas.manuel1115@yahoo.com>
2026-07-22 13:14:06 -0700

    Update admin.js

--- admin.js
+++ admin.js
@@ -2,7 +2,9 @@ import { db } from "./firebase.js";
 
 import {
     collection,
-    getDocs
+    getDocs,
+    doc,
+    setDoc
 } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-firestore.js";
 
 const loginScreen =
